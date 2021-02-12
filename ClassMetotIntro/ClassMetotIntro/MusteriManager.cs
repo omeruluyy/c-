e536d80585f1f78a625deffbc288afd47c795514
omeruluyy <70287722+omeruluyy@users.noreply.github.com>
2021-02-12 21:14:22 +0300

    Add files via upload

---
+++ ClassMetotIntro/ClassMetotIntro/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotIntro
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi :" + musteri.Ad);
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Musteri silindi :" + musteri.Ad);
+
+
+        }
+        public void List(Musteri[] musteriList)
+        {
+            foreach (var item in musteriList)
+            {
+                Console.WriteLine(item.Ad);
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
