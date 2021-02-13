beb576d9b41513e4ca23df194c6aa26c7b17a0c7
omeruluyy <70287722+omeruluyy@users.noreply.github.com>
2021-02-13 16:15:55 +0300

    Add files via upload

---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> sehirler1 = new Dictionary<string, int>();
+            sehirler1.Add("Kayseri", 38);
+            MyDict<string, int> sehirler2 = new MyDict<string, int>();
+            sehirler2.Add("Kayseri", 38);
+        }
+    }
+}
